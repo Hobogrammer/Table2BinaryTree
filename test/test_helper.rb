5f6694c7f6e2990e913a2a5955942f521432299a
Andre Smith <andrepsmithjr@gmail.com>
2014-02-28 17:21:54 -0800

    Write all the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,22 @@
 require 'minitest/autorun'
 require 'minitest/spec'
+
+def create_tree
+  b = Tree.new("b")
+  g = Tree.new("g")
+  f = Tree.new("f", b, g)
+  a = Tree.new("a")
+  b.left = a
+  d = Tree.new("d")
+  b.right = d
+  c = Tree.new("c")
+  e = Tree.new("e")
+  d.left = c
+  d.right = e
+
+  h = Tree.new("h")
+  i = Tree.new("i", h)
+  g.right = i
+
+  return f
+end
--- test/tree_test.rb
+++ test/tree_test.rb
@@ -1,6 +1,32 @@
 require 'test_helper'
+require 'tree'
 
-describe "Tree" do
-  it "should return for " do
+describe "In order tree traversal" do
+  it "should return A,B,C,D,E,F,G,H,I for F,B,A,D,C,E,G,I,H" do
+    root = create_tree
+
+    list = traverse(root,"in_order_traverse")
+
+    list.must_equal "a,b,c,d,e,f,g,h,i,"
+  end
+end
+
+describe "Post order tree traversal" do
+  it "should return A,C,E,D,B,H,I,G,F for F,B,A,D,C,E,G,I,H" do
+    root = create_tree
+
+    list = traverse(root,"post_order_traverse")
+
+    list.must_equal "a,c,e,d,b,h,i,g,f,"
+  end
+end
+
+describe "Pre order tree traversal" do
+  it "should return F,B,A,D,C,E,G,I,H for F,B,A,D,C,E,G,I,H" do
+    root = create_tree
+
+    list = traverse(root,"preorder_traverse")
+
+    list.must_equal "f,b,a,d,c,e,g,i,h,"
   end
 end
